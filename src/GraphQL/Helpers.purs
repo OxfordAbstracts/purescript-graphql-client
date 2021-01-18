4e736d08058c553ec635c38eccce170b18f88386
roryc89 <roryc89@googlemail.com>
2021-01-18 19:18:38 +0100

    deletes unused helpers module

--- src/GraphQL/Helpers.purs
+++
@@ -1,9 +0,0 @@
-module GraphQl.Helpers where
-
-import Prelude
-
-import Data.Newtype (class Newtype, unwrap)
-import Data.String (joinWith)
-
-toPgIdArray :: forall a. Newtype a Int => Array a -> String
-toPgIdArray ns = "{" <> (joinWith "," $ map (unwrap >>> show) ns) <> "}"
