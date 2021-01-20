302470b3bee918a595caa623c0b9753ec7df3f2c
roryc89 <roryc89@googlemail.com>
2021-01-20 14:14:25 +0100

    adds cache file

---
+++ codegen/schema/test/cache.js
@@ -0,0 +1,27 @@
+const fs = require('fs')
+const { promisify } = require('util')
+const write = promisify(fs.writeFile)
+const read = promisify(fs.readFile)
+const mkdirp = require('mkdirp')
+var crypto = require('crypto')
+
+mkdirp('./cache')
+
+exports.set = async ({ key, val }) =>
+  write(fileName(key), JSON.stringify(val))
+
+exports.get = async (key) => {
+  try {
+    const json = (await read(fileName(key))).toString()
+    return JSON.parse(json)
+  } catch (e) {
+    return null
+  }
+}
+
+const fileName = key => `./cache/${hash(key)}`
+
+const hash = key => crypto
+  .createHash('md5')
+  .update(JSON.stringify(key))
+  .digest('hex')
