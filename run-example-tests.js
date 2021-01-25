c1293b5cb288448d846b3ccacb1ab04fe4afd156
Rory Campbell <roryc89@googlemail.com>
2021-01-25 22:59:56 +0100

    add e2e/example tests

--- run-example-tests.js
+++ run-example-tests.js
@@ -1,1 +1,21 @@
-console.log('hello e2e')
+const { readdirSync } = require('fs')
+const {promisify} = require('util')
+const exec = promisify(require("exec-sh"))
+
+const getDirectories = source =>
+  readdirSync(source, { withFileTypes: true })
+    .filter(dirent => dirent.isDirectory())
+    .map(dirent => dirent.name)
+
+const go = async () => {
+
+    const packages = getDirectories('./examples');
+
+    console.log(packages)
+
+    for(let package of packages){
+        await exec(`cd "./examples/${package}" && npm t`)
+    }
+}
+
+go()
