f74357df9c330e6c7dd3a769f39701a3cfcd9b1a
roryc89 <roryc89@googlemail.com>
2021-01-26 20:20:48 +0100

    adds should fail tests

--- run-example-tests.js
+++ run-example-tests.js
@@ -1,6 +1,6 @@
 const { readdirSync } = require('fs')
-const {promisify} = require('util')
-const exec = promisify(require("exec-sh"))
+const { promisify } = require('util')
+const exec = promisify(require('exec-sh'))
 
 const getDirectories = source =>
   readdirSync(source, { withFileTypes: true })
@@ -8,13 +8,12 @@ const getDirectories = source =>
     .map(dirent => dirent.name)
 
 const go = async () => {
+  const packages = getDirectories('./examples')
 
-    const packages = getDirectories('./examples');
-
-    for(let package of packages){
-        console.log(`Testing ${package}`);
-        await exec(`cd "./examples/${package}" && npm t`)
-    }
+  for (const p of packages) {
+    console.log(`Testing ${p}`)
+    await exec(`cd "./examples/${p}" && npm t`)
+  }
 }
 
 go()
