88c1167d65132860b4b5a3953469e2a7d0089eb2
roryc89 <roryc89@googlemail.com>
2021-01-27 21:32:35 +0100

    start urql implementation

--- src/GraphQL/Client/Query.purs
+++ src/GraphQL/Client/Query.purs
@@ -1,10 +1,12 @@
-module GraphQL.Client.Query (class GqlQuery, query, queryWithDecoder, mutation, mutationWithDecoder) where
+module GraphQL.Client.Query where
+
+-- module GraphQL.Client.Query (class GqlQuery, query, queryWithDecoder, mutation, mutationWithDecoder) where
 
 import Prelude
 
 import Affjax (Error, Response, URL, defaultRequest, printError, request)
 import Affjax.RequestBody as RequestBody
-import Affjax.RequestHeader (RequestHeader(..))
+import Affjax.RequestHeader (RequestHeader(..), name, value)
 import Affjax.ResponseFormat as ResponseFormat
 import Control.Monad.Except (catchError)
 import Data.Argonaut.Core (Json)
@@ -15,11 +17,21 @@ import Data.Either (Either(..))
 import Data.HTTP.Method as Method
 import Data.Maybe (Maybe(..))
 import Data.MediaType.Common (applicationJSON)
+import Data.Nullable (Nullable, toNullable)
 import Data.String.CodeUnits (fromCharArray, toCharArray)
+import Data.Tuple (Tuple(..))
+import Effect (Effect)
 import Effect.Aff (Aff, error, message, throwError)
+import Effect.Aff.Compat (EffectFnAff, fromEffectFnAff)
+import Effect.Class (liftEffect)
+import Effect.Class.Console (info, infoShow, log, logShow)
+import Foreign (Foreign)
+import Foreign.Object (Object)
+import Foreign.Object as Object
+import Global.Unsafe (unsafeStringify)
 import GraphQL.Client.QueryReturns (class QueryReturns)
 import GraphQL.Client.ToGqlString (class GqlQueryString, toGqlQueryString, toGqlQueryStringFormatted)
-import Type.Proxy (Proxy)
+import Type.Proxy (Proxy(..))
 import Unsafe.Coerce (unsafeCoerce)
 
 class
@@ -33,6 +45,36 @@ instance queriable ::
   ) =>
   GqlQuery schema query returns
 
+type Options schema returns
+  = { url :: URL
+    , headers :: Array RequestHeader
+    , decode :: Json -> Either JsonDecodeError returns
+    , schema :: Proxy schema
+    , clientId :: String
+    }
+
+defaultOptions ::
+  forall schema returns.
+  DecodeJson returns => Options schema returns
+defaultOptions =
+  { url: ""
+  , headers: []
+  , decode: decodeJson
+  , schema: Proxy
+  , clientId: ""
+  }
+
+data Operation
+  = Query
+  | Mutation
+  | Subscription
+
+-- query ::
+--   forall returns query schema.
+--   GqlQuery schema query returns =>
+--   Options schema returns -> String -> query -> Aff returns
+-- query opts queryName q = do
+--   runOperation Query opts.decode (Proxy :: Proxy schema) opts.url opts.headers queryName q
 query ::
   forall returns query schema.
   GqlQuery schema query returns =>
@@ -65,25 +107,40 @@ runOperation ::
   Operation -> (Json -> Either JsonDecodeError returns) -> Proxy schema -> URL -> Array RequestHeader -> String -> query -> Aff returns
 runOperation operation decodeFn _ url headers queryNameUnsafe q =
   addErrorInfo do
-    res <- queryPostForeign operation url headers queryName $ toGqlQueryString q
-    case res of
+    info "START runOperation"
+    client <- liftEffect $ createClient { headers, url, websocketUrl: Nothing }
+    info "GOT CLIENT"
+    json <- query_ client $ toGqlQueryString q
+    info "JSON: "
+    info $ unsafeStringify json
+    case decodeData json of
       Left err ->
         throwError
-          $ error
-          $ "Response failed to decode to JSON: "
-          <> printError err
-      Right { body } -> case decodeData body of
-        Left err ->
-          throwError
-            $ error case decodeJson body of
-                Right ({ errors } :: { errors :: Array { message :: String } }) -> intercalate ", \n" $ map _.message errors
-                _ ->
-                  " Response failed to decode from JSON: "
-                    <> printJsonDecodeError err
-        Right result -> pure result
+          $ error case decodeJson json of
+              Right ({ errors } :: { errors :: Array { message :: String } }) -> intercalate ", \n" $ map _.message errors
+              _ ->
+                " Response failed to decode from JSON: "
+                  <> printJsonDecodeError err
+      Right result -> pure result
+  -- res <- queryPostForeign operation url headers queryName $ toGqlQueryString q
+  -- case res of
+  --   Left err ->
+  --     throwError
+  --       $ error
+  --       $ "Response failed to decode to JSON: "
+  --       <> printError err
+  --   Right { body } -> case decodeData body of
+  --     Left err ->
+  --       throwError
+  --         $ error case decodeJson body of
+  --             Right ({ errors } :: { errors :: Array { message :: String } }) -> intercalate ", \n" $ map _.message errors
+  --             _ ->
+  --               " Response failed to decode from JSON: "
+  --                 <> printJsonDecodeError err
+  --     Right result -> pure result
   where
   queryName =
-    queryNameUnsafe 
+    queryNameUnsafe
       # toCharArray
       <#> (\ch -> if ch == ' ' then '_' else ch)
       # filter (isAlphaNum || eq '_')
@@ -109,37 +166,62 @@ runOperation operation decodeFn _ url headers queryNameUnsafe q =
     data_ <- getField jsonObj "data"
     decodeFn data_
 
-queryPostForeign ::
-  Operation -> URL -> Array RequestHeader -> String -> String -> Aff (Either Error (Response Json))
-queryPostForeign operation url headers queryName q =
-  request
-    defaultRequest
-      { withCredentials = true
-      , url = url
-      , method = Left Method.POST
-      , responseFormat = ResponseFormat.json
-      , content =
-        Just
-          $ RequestBody.Json
-          $ toJson
-              { query: opStr <> " " <> queryName <> " " <> q
-              , variables: {}
-              , operationName: queryName
-              }
-      , headers = headers <> [ ContentType applicationJSON ]
-      }
+-- queryPostForeign ::
+--   Operation -> URL -> Array RequestHeader -> String -> String -> Aff (Either Error (Response Json))
+-- queryPostForeign operation url headers queryName q =
+--   request
+--     defaultRequest
+--       { withCredentials = true
+--       , url = url
+--       , method = Left Method.POST
+--       , responseFormat = ResponseFormat.json
+--       , content =
+--         Just
+--           $ RequestBody.Json
+--           $ toJson
+--               { query: opStr <> " " <> queryName <> " " <> q
+--               , variables: {}
+--               , operationName: queryName
+--               }
+--       , headers = headers <> [ ContentType applicationJSON ]
+--       }
+--   where
+--   opStr = case operation of
+--     Query -> "query"
+--     Mutation -> "mutation"
+--     Subscription -> "subscription"
+
+--   toJson ::
+--     { operationName :: String
+--     , query :: String
+--     , variables :: {}
+--     } ->
+--     Json
+--   toJson = unsafeCoerce
+
+foreign import data GraphQlClient :: Type
+
+createClient ::
+  { url :: URL
+  , websocketUrl :: Maybe String
+  , headers :: Array RequestHeader
+  } ->
+  Effect GraphQlClient
+createClient opts = createClientImpl opts 
+  { headers = Object.fromFoldable $ map toTup opts.headers 
+  , websocketUrl = toNullable opts.websocketUrl
+  }
   where
-  opStr = case operation of 
-    Query -> "query"
-    Mutation -> "mutation"
-    Subscription -> "subscription"
-  toJson ::
-    { operationName :: String
-    , query :: String
-    , variables :: {}
-    } ->
-    Json
-  toJson = unsafeCoerce
+  toTup header = Tuple (name header) (value header)
 
+foreign import createClientImpl ::
+  { url :: URL
+  , websocketUrl :: Nullable String
+  , headers :: Object String
+  } ->
+  Effect GraphQlClient
 
-data Operation = Query | Mutation | Subscription
+query_ :: GraphQlClient -> String -> Aff Json
+query_ client str = fromEffectFnAff $ queryImpl client str
+
+foreign import queryImpl :: GraphQlClient -> String -> EffectFnAff Json
