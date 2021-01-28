45ae62ecc77923aaaedd6bccbac4f9b03c7058bb
roryc89 <roryc89@googlemail.com>
2021-01-28 15:24:27 +0100

    removes logs

--- examples/1-simple/src/Main.purs
+++ examples/1-simple/src/Main.purs
@@ -6,7 +6,7 @@ import Data.Argonaut.Decode (class DecodeJson)
 import Data.Symbol (SProxy(..))
 import Effect (Effect)
 import Effect.Aff (Aff, launchAff_)
-import Effect.Class.Console (infoShow, logShow)
+import Effect.Class.Console (logShow)
 import GraphQL.Client.Args (type (==>), (=>>))
 import GraphQL.Client.Query (class GqlQuery, query_)
 import Type.Proxy (Proxy(..))
@@ -14,11 +14,9 @@ import Type.Proxy (Proxy(..))
 main :: Effect Unit
 main =
   launchAff_ do
-    infoShow "START 1"
     { widgets } <-
       queryGql "Widget names with id 1"
         { widgets: { id: 1 } =>> { name } }
-    infoShow widgets
     logShow $ map _.name widgets
 
 -- Run gql query
