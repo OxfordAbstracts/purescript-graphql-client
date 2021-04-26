9e904ab0a4520e444253c9e8fe7bb8199b71dfc1
roryc89 <roryc89@googlemail.com>
2021-04-26 09:54:23 +0100

    remove unsafe-coerce

--- spago.dhall
+++ spago.dhall
@@ -54,7 +54,7 @@ You can edit this file as you like.
   , "type-equality"
   , "typelevel"
   , "unicode"
-  , "unsafe-coerce"
+  -- , "unsafe-coerce"
   ]
 , packages = ./packages.dhall
 , sources = [ "src/**/*.purs", "test/**/*.purs" ]
--- test/GraphQL/Hasura/Decode.purs
+++ test/GraphQL/Hasura/Decode.purs
@@ -2,6 +2,7 @@ module GraphQL.Hasura.Decode.Test where
 
 import Prelude
 
+import Data.Argonaut.Core (fromString)
 import Data.DateTime (DateTime(..), Time(..), canonicalDate)
 import Data.Either (Either(..))
 import Data.Enum (class BoundedEnum, toEnum)
@@ -9,14 +10,13 @@ import Data.Maybe (fromMaybe)
 import GraphQL.Hasura.Decode (decodeHasura)
 import Test.Spec (Spec, describe, it)
 import Test.Spec.Assertions (shouldEqual)
-import Unsafe.Coerce (unsafeCoerce)
 
 spec :: Spec Unit
 spec =
   describe " GraphQL.Hasura.Decode" do
     describe "decodeHasura" do
       it "decodes a date time without milliseconds"
-        $ decodeHasura (unsafeCoerce "2020-10-09T08:07:06")
+        $ decodeHasura (fromString "2020-10-09T08:07:06")
             `shouldEqual`
               ( Right
                   $ DateTime
@@ -24,7 +24,7 @@ spec =
                       (Time (toEnumB 8) (toEnumB 7) (toEnumB 6) (toEnumB 0))
               )
       it "decodes a date time with milliseconds"
-        $ decodeHasura (unsafeCoerce "2020-10-09T08:07:06.555")
+        $ decodeHasura (fromString "2020-10-09T08:07:06.555")
             `shouldEqual`
               ( Right
                   $ DateTime
@@ -32,7 +32,7 @@ spec =
                       (Time (toEnumB 8) (toEnumB 7) (toEnumB 6) (toEnumB 555))
               )
       it "decodes a date time with microseconds"
-        $ decodeHasura (unsafeCoerce "2020-10-09T08:07:06.555555")
+        $ decodeHasura (fromString "2020-10-09T08:07:06.555555")
             `shouldEqual`
               ( Right
                   $ DateTime
@@ -40,7 +40,7 @@ spec =
                       (Time (toEnumB 8) (toEnumB 7) (toEnumB 6) (toEnumB 555))
               )
       it "decodes a date time with a positive timezone"
-        $ decodeHasura (unsafeCoerce "2020-10-09T08:07:06.555555+01:00")
+        $ decodeHasura (fromString "2020-10-09T08:07:06.555555+01:00")
             `shouldEqual`
               ( Right
                   $ DateTime
@@ -48,7 +48,7 @@ spec =
                       (Time (toEnumB 7) (toEnumB 7) (toEnumB 6) (toEnumB 555))
               )
       it "decodes a date time with a negative timezone"
-        $ decodeHasura (unsafeCoerce "2020-10-09T08:00:00.555555-02:30")
+        $ decodeHasura (fromString "2020-10-09T08:00:00.555555-02:30")
             `shouldEqual`
               ( Right
                   $ DateTime
