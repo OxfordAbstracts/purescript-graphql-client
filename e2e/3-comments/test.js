4f1510fe7443b68818c4fa0e4740fdbaf2769137
roryc89 <roryc89@googlemail.com>
2021-05-11 15:02:00 +0100

    completes comments test

---
+++ e2e/3-comments/test.js
@@ -0,0 +1,29 @@
+const { deepStrictEqual } = require('assert')
+const exec = require('exec-sh').promise
+const logs = []
+
+console.log = (log) => {
+  console.info(log)
+  logs.push(log)
+}
+
+require('./server-fn')(async () => {
+  try {
+    await require('./generate-purs-schema')()
+    await exec('npm run build', { stdio: 'pipe', stderr: 'pipe' })
+    require('./output/Main').main()
+    setTimeout(() => {
+      deepStrictEqual(logs, ['[RED]'])
+      console.info('tests passed')
+      process.exit(0)
+    }, 250)
+  } catch (err) {
+    console.error('test error', err)
+    process.exit(1)
+  }
+})
+
+setTimeout(() => {
+  console.error('Timeout')
+  process.exit(1)
+}, 60000)
