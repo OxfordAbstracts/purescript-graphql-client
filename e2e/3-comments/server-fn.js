3e2e1f2c391acb911fe07c3f833405f081909475
roryc89 <roryc89@googlemail.com>
2021-05-11 14:54:09 +0100

    fixes comments breaking schema gen

---
+++ e2e/3-comments/server-fn.js
@@ -0,0 +1,52 @@
+
+module.exports = (onListening) => {
+  const express = require('express')
+  const { graphqlHTTP } = require('express-graphql')
+  const { buildSchema } = require('graphql')
+
+  const schema = buildSchema(`
+    type Query {
+        prop: String
+        widgets(colour: Colour): [Widget!]!
+    }
+    #Comment here
+    type Widget {
+        id: Int
+        name: String!
+        colour: Colour!
+    }
+    "colour description"
+    enum Colour {
+      "red description"
+      RED
+      GREEN
+      BLUE
+      yellow
+    }
+
+    `)
+
+  const root = {
+    prop: () => {
+      return 'Hello world!'
+    },
+    widgets: ({ colour }) =>
+      widgets.filter(w => !colour || colour === w.colour)
+
+  }
+
+  const widgets = [
+    { id: 1, name: 'one', colour: 'RED' },
+    { id: 2, name: 'two', colour: 'GREEN' }
+  ]
+
+  const app = express()
+
+  app.use('/graphql', graphqlHTTP({
+    schema,
+    rootValue: root,
+    graphiql: true
+  }))
+
+  app.listen(4000, onListening)
+}
