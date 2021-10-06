3e0637d5b5b4f705f5b4e1c0a078387c6254e3c3
roryc89 <roryc89@googlemail.com>
2021-10-06 23:25:10 +0100

    rms generated files

--- examples/9-variables/src/generated/Symbols.purs
+++
@@ -1,9 +0,0 @@
-module Generated.Gql.Symbols where
-
-import Type.Proxy (Proxy(..))
-
-colour = Proxy :: Proxy"colour"
-id = Proxy :: Proxy"id"
-name = Proxy :: Proxy"name"
-prop = Proxy :: Proxy"prop"
-widgets = Proxy :: Proxy"widgets"
