4d88b068e557f6b931dddbda08d7490c7514fe63
roryc89 <roryc89@googlemail.com>
2021-10-18 10:56:44 +0100

    fixes more tests to by including directives type #61

--- examples/9-variables/src/Main.purs
+++ examples/9-variables/src/Main.purs
@@ -1,6 +1,7 @@
 module Main where
 
 import Prelude
+
 import Data.Argonaut.Decode (class DecodeJson)
 import Effect (Effect)
 import Effect.Aff (Aff, launchAff_)
@@ -9,10 +10,12 @@ import Generated.Gql.Enum.Colour (Colour(..))
 import Generated.Gql.Schema.Admin (Query)
 import Generated.Gql.Symbols (colour)
 import GraphQL.Client.Args ((=>>))
+import GraphQL.Client.Operation (OpQuery)
 import GraphQL.Client.Query (query_)
 import GraphQL.Client.Types (class GqlQuery)
 import GraphQL.Client.Variable (Var(..))
 import GraphQL.Client.Variables (withVars)
+import Type.Data.List (Nil')
 import Type.Proxy (Proxy(..))
 
 main :: Effect Unit
