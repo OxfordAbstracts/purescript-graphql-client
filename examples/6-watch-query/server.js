e3dfe221980154cfcc1d4a16c530d8d708a1c44c
Rory Campbell <roryc89@googlemail.com>
2022-08-01 21:11:57 +0100

    update other subscription server fns

--- examples/6-watch-query/server.js
+++ examples/6-watch-query/server.js
@@ -1,5 +1,5 @@
 import serverFn from './server-fn.js'
-serverFn((listener, server, PORT) => {
+serverFn((server, PORT) => {
   console.log(`🚀 Server ready at http://localhost:${PORT}${server.graphqlPath}`)
   console.log(`🚀 Subscriptions ready at ws://localhost:${PORT}${server.subscriptionsPath}`)
 })
