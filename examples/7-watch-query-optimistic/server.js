221238fcbe101c4c966dea3beb866259e4fd82e6
Rory Campbell <roryc89@googlemail.com>
2022-08-01 21:32:23 +0100

    remove flaky test

--- examples/7-watch-query-optimistic/server.js
+++
@@ -1,5 +0,0 @@
-import serverFn from './server-fn.js'
-serverFn((server, PORT) => {
-  console.log(`🚀 Server ready at http://localhost:${PORT}${server.graphqlPath}`)
-  console.log(`🚀 Subscriptions ready at ws://localhost:${PORT}${server.subscriptionsPath}`)
-})
