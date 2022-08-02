99993134cc4b613648dd64da8b34391cb5d4958d
Rory Campbell <roryc89@googlemail.com>
2022-08-02 22:33:16 +0100

    adds decodeJsonUnionWith

--- src/GraphQL/Client/Union.purs
+++ src/GraphQL/Client/Union.purs
@@ -5,11 +5,11 @@ import Prelude
 import Data.Argonaut.Core (Json, caseJsonObject, fromObject, fromString, toString)
 import Data.Argonaut.Decode (class DecodeJson, JsonDecodeError(..))
 import Data.Argonaut.Decode.Class (decodeJson)
-import Data.Variant (Variant, expand, inj)
 import Data.Either (Either(..))
 import Data.Maybe (Maybe(..))
 import Data.Newtype (class Newtype)
 import Data.Symbol (class IsSymbol, reflectSymbol)
+import Data.Variant (Variant, expand, inj)
 import Foreign.Object (lookup)
 import Prim.Row (class Cons, class Lacks, class Union)
 import Prim.RowList as RL
@@ -20,17 +20,25 @@ newtype GqlUnion r = GqlUnion (Record r)
 derive instance newtypeGqlUnion :: Newtype (GqlUnion r) _
 
 newtype UnionReturned r = UnionReturned (Variant r)
+
 derive instance newtypeGqlUnionReturned :: Newtype (UnionReturned r) _
 
 instance (RL.RowToList r rl, DecodeUnion rl r) => DecodeJson (UnionReturned r) where
-  decodeJson =
-    caseJsonObject (Left $ TypeMismatch "object") \obj ->
-      case lookup __typename obj of
-        Nothing -> Left $ AtKey __typename $ MissingValue
-        Just ty -> case toString ty of
-          Nothing -> Left $ AtKey __typename $ TypeMismatch "string"
-          Just ty' -> map UnionReturned $
-            decodeUnion ty' (fromObject obj) (Proxy :: Proxy rl)
+  decodeJson = decodeJsonUnionWith (decodeUnion :: _ -> _ -> Proxy rl -> _)
+
+decodeJsonUnionWith
+  :: forall (rl :: RL.RowList Type) (r :: Row Type)
+   . (String -> Json -> Proxy rl -> Either JsonDecodeError (Variant r))
+  -> Json
+  -> Either JsonDecodeError (UnionReturned r)
+decodeJsonUnionWith decode =
+  caseJsonObject (Left $ TypeMismatch "object") \obj ->
+    case lookup __typename obj of
+      Nothing -> Left $ AtKey __typename $ MissingValue
+      Just ty -> case toString ty of
+        Nothing -> Left $ AtKey __typename $ TypeMismatch "string"
+        Just ty' -> map UnionReturned $
+          decode ty' (fromObject obj) (Proxy :: Proxy rl)
 
 class DecodeUnion (rl :: RL.RowList Type) r where
   decodeUnion :: String -> Json -> Proxy rl -> Either JsonDecodeError (Variant r)
@@ -45,7 +53,7 @@ instance
   , Union r rd r'
   ) =>
   DecodeUnion (RL.Cons l ty rl) r' where
-  decodeUnion ty json _ =
+  decodeUnion ty json _ = 
     if reflectSymbol l == ty then map (inj l) $ decodeJson json
     else map expand (decodeUnion ty json (Proxy :: Proxy rl) :: Either JsonDecodeError (Variant r))
     where
