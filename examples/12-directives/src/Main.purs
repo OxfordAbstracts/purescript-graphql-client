89646ceaeae1f559e450c7578e7a18cb95489403
Rory Campbell <roryc89@googlemail.com>
2023-03-04 09:28:57 +0700

    adds satifsy not null constraint for directives #61

--- examples/12-directives/src/Main.purs
+++ examples/12-directives/src/Main.purs
@@ -18,7 +18,7 @@ main =
   launchAff_ do
     { widgets } <-
       queryGql "widgets_cached"
-        $ cached { ttl: 10, refresh: false }
+        $ cached { refresh: false }
             { widgets: { id: 1 } =>> { name: unit }
             }
     logShow $ map _.name widgets
