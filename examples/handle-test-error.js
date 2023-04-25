00fbb5cd0461db98b83b227080433ca4db1980ae
Rory Campbell <roryc89@googlemail.com>
2023-04-25 21:06:28 +0100

    adds handleTestError

---
+++ examples/handle-test-error.js
@@ -0,0 +1,7 @@
+export const handleTestError = (err) => {
+  console.err(`Test error`, err);
+  if (err.message.length > 2000) {
+    console.err('error end', err.message.slice(-2000));
+  }
+  process.exit(1);
+};
