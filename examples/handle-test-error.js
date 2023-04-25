930c8aa79d5d2b4bf1a3be3c7de4b8ae2b1a3bd3
Rory Campbell <roryc89@googlemail.com>
2023-04-25 21:39:34 +0100

    console.error

--- examples/handle-test-error.js
+++ examples/handle-test-error.js
@@ -1,5 +1,5 @@
 exports.handleTestError = (err) => {
-  console.err(`Test error`, err);
+  console.error(`Test error`, err);
   if (err.message.length > 2000) {
     console.error('error end', err.message.slice(-2000));
   }
