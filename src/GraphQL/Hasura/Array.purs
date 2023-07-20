32723f34a53492b3f8876de05607b287cb6d13e5
Rory Campbell <roryc89@googlemail.com>
2023-07-20 11:03:37 +0100

    adds VarTypeName Hasura_text

--- src/GraphQL/Hasura/Array.purs
+++ src/GraphQL/Hasura/Array.purs
@@ -6,6 +6,7 @@ import Data.Newtype (class Newtype, unwrap)
 import Data.String (joinWith)
 import GraphQL.Client.Args (class ArgGql)
 import GraphQL.Client.ToGqlString (class GqlArgString)
+import GraphQL.Client.Variables.TypeName (class VarTypeName)
 import GraphQL.Hasura.Decode (class DecodeHasura)
 import GraphQL.Hasura.Encode (class EncodeHasura)
 
@@ -28,3 +29,6 @@ instance GqlArgString Hasura_text where
   toGqlArgStringImpl = unwrap >>> map show >>> joinWith "," >>> (\s -> "{" <> s <> "}") >>> show
 
 instance ArgGql Hasura_text Hasura_text
+
+instance VarTypeName Hasura_text where 
+  varTypeName _ = "_text"
