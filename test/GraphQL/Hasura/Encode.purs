6d81aed5657bd2a2ff9d7e8d7d47c64c9b6a3b5c
Rory Campbell <roryc89@googlemail.com>
2023-07-27 15:50:42 +0100

    gives hasura text correct encoding for variables

--- test/GraphQL/Hasura/Encode.purs
+++ test/GraphQL/Hasura/Encode.purs
@@ -7,6 +7,7 @@ import Data.DateTime.Gen (genDateTime)
 import Data.Either (Either(..))
 import Data.Maybe (Maybe)
 import Effect.Class (liftEffect)
+import GraphQL.Hasura.Array (Hasura_text)
 import GraphQL.Hasura.Decode (class DecodeHasura, decodeHasura)
 import GraphQL.Hasura.Encode (class EncodeHasura, encodeHasura)
 import Test.QuickCheck (class Arbitrary, quickCheck, (===))
@@ -17,10 +18,15 @@ spec =
   describe "GraphQL.Hasura.Encode.Test" do
     describe "encodeHasura and decodeHasura" do
       it "all values encoded by encodeHasura should be successfully decoded by decodeHasura" do
-        pure unit
         liftEffect
           $ quickCheck \(input :: Array Input) ->
               decodeHasura (encodeHasura input) === Right input
+
+      describe "Hasura_text" do 
+        it "should encode and decode Hasura_text" do
+          liftEffect
+            $ quickCheck \(input :: Hasura_text) ->
+                decodeHasura (encodeHasura input) === Right input
 
 type Input
   = { string :: String
@@ -32,6 +38,7 @@ type Input
     , numberM :: Maybe Number
     , datetime :: ArbDateTime
     , datetimes :: Array ArbDateTime
+    -- , hasura_text :: Hasura_text
     }
 
 newtype ArbDateTime
