0e52656991b97346793569fca9a64948e4fe8307
Brad Reed <brad@bradreed.co.uk>
2025-11-26 22:40:34 +0000

    update some npm dependencies and remove some others

--- codegen/schema/index.mjs
+++ codegen/schema/index.mjs
@@ -1,27 +1,29 @@
-import { writePursSchemas } from './write-purs-schema.mjs'
-import { getGqlSchema } from './get-gql-schema.mjs'
-import { promisify } from 'util'
-import mkdirp from 'mkdirp'
-import rimraf from 'rimraf'
-const rm = promisify(rimraf)
+import fs from "node:fs/promises";
+import { rimraf } from "rimraf";
+import { writePursSchemas } from "./write-purs-schema.mjs";
+import { getGqlSchema } from "./get-gql-schema.mjs";
 
-export async function generateSchemas  (opts, gqlEndpoints) {
+const mkdirp = (path) => fs.mkdir(path, { recursive: true });
+
+export async function generateSchemas(opts, gqlEndpoints) {
   if (!Array.isArray(gqlEndpoints)) {
-    gqlEndpoints = [gqlEndpoints]
+    gqlEndpoints = [gqlEndpoints];
   }
-  await rm(opts.dir)
-  await mkdirp(opts.dir)
-  await mkdirp(opts.dir + '/Schema')
-  await mkdirp(opts.dir + '/Enum')
-  await mkdirp(opts.dir + '/Directives')
-  const schemas = await Promise.all(gqlEndpoints.map(getGqlSchema))
+  await rimraf(opts.dir);
+  await mkdirp(opts.dir);
+  await mkdirp(opts.dir + "/Schema");
+  await mkdirp(opts.dir + "/Enum");
+  await mkdirp(opts.dir + "/Directives");
+  const schemas = await Promise.all(gqlEndpoints.map(getGqlSchema));
 
-  return await writePursSchemas(opts, schemas)
+  return await writePursSchemas(opts, schemas);
 }
 
-export async function generateSchema (opts) {
-  const { modulePath, url } = opts
-  const moduleName = modulePath[modulePath.length - 1]
+export async function generateSchema(opts) {
+  const { modulePath, url } = opts;
+  const moduleName = modulePath[modulePath.length - 1];
 
-  return generateSchemas({ ...opts, modulePath: modulePath.slice(0, -1) }, [{ moduleName, url }])
+  return generateSchemas({ ...opts, modulePath: modulePath.slice(0, -1) }, [
+    { moduleName, url },
+  ]);
 }
