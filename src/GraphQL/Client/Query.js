dcca416c881a3c7ba6a244a22ee625df03b0fd73
Brad Reed <brad@bradreed.co.uk>
2026-03-10 20:54:31 +0800

    add more info about errors in Query.js

--- src/GraphQL/Client/Query.js
+++ src/GraphQL/Client/Query.js
@@ -1,3 +1,17 @@
-export function cause(err) {
-  return String(err.cause || "");
-}
+const formatError = (err) => {
+  if (!err) return "";
+  const props = ["message", "code", "syscall", "hostname", "address", "port"]
+    .filter((k) => err[k])
+    .map((k) => `${k}: ${err[k]}`);
+  if (err.cause) props.push(`cause: ${formatError(err.cause)}`);
+  return props.join(", ");
+};
+
+export const cause = (err) => {
+  const c = err.cause;
+  if (!c) return "";
+  if (c instanceof AggregateError) {
+    return [c.message, ...Array.from(c.errors, (e, i) => `[${i}] ${formatError(e)}`)].join("\n");
+  }
+  return formatError(c);
+};
